1bdcc24d4a9c8af5a1b40fd8754adeac847eab79
GiuSposito <gsposito@ciandt.com>
2021-03-18 12:31:18 -0300

    Cricao do post sobre chilling effects

---
+++ content/post/bit-by-bit-chilling-effect/Penney_2016.R
@@ -0,0 +1,5 @@
+# Penney, Jonathon. 2016. “Chilling Effects: Online Surveillance and Wikipedia Use.”
+# Berkeley Technology Law Journal 31 (1): 117. doi:10.15779/Z38SS13.
+# https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2769645
+
+
